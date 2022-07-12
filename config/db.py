7c3d6b0ad125b1f91646b117b7f11b57e58bdf47
Neil Cabanillas <neil.cabanillas@gmail.com>
2022-07-12 16:24:04 -0500

    Se sube proyecto de crud con mysql del usuario

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root:password@localhost:3306/storedb")
+engine = create_engine("mysql+pymysql://root:Exitos2023#@localhost:3306/storedb")
 
 meta = MetaData()
 
